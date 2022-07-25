773c1c0b1685597ed53a004e9602d41755f7e2cf
Himanshu Raj <himanshurajverma549@gmail.com>
2022-07-25 16:00:24 +0530

    bug fixed

--- formSubmission.js
+++ formSubmission.js
@@ -1,6 +1,8 @@
 const submitForm = () => {
     let formElm = document.querySelector("[data-form='form']")
     formElm.addEventListener('submit', (e) => {
+        e.preventDefault()
+        e.stopPropagation()
         e.target.querySelector("input[type='password']").type = "text"
         document.location = '/dashboard';
     })
