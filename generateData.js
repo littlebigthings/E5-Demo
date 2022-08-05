2de116e21e884544de947d1a5f2b872d274b46b6
Himanshu Raj <himanshurajverma549@gmail.com>
2022-08-05 16:26:09 +0530

    added one more dropdonw dta

--- generateData.js
+++ generateData.js
@@ -317,6 +317,69 @@ const AnalyticsData = {
             length: 11,
         },
     },
+    AlertManagement: {
+        TransactionsCompleted: {
+            numberOfTransctionsRange: {
+                min: 2000,
+                max: 7470,
+            },
+            percentFromLastRange: {
+                min: 10,
+                max: 40,
+            },
+        },
+        NeedsInterventions: {
+            numberOfTransctionsRange: {
+                min: 200,
+                max: 400,
+            },
+            percentFromLastRange: {
+                min: 2,
+                max: 15,
+            },
+        },
+        haveSaved: {
+            hours: {
+                min: 50,
+                max: 200,
+            },
+            money: {
+                min: 10990,
+                max: 12990,
+            },
+            FTE: {
+                min: 2.5,
+                max: 6.6,
+            }
+        },
+        ThroughputPerHour: {
+            transactions: {
+                min: 233,
+                max: 600,
+            },
+            avgMinTransctions: {
+                min: 1,
+                max: 10,
+            }
+        },
+        chartOneDataPoints: {
+            byDay:{
+                rangeMin: 100,
+                rangeMax: 400,
+                length: 30,
+            },
+            byWeek:{
+                rangeMin: 200,
+                rangeMax: 400,
+                length: 30,
+            }
+        },
+        chartTwoDataPoints: {
+            rangeMin: 0,
+            rangeMax: 50,
+            length: 11,
+        },
+    },
 }
 const WorkFlowSummeryData = {
     RCD: {
@@ -398,6 +461,32 @@ const WorkFlowSummeryData = {
         }
     },
     EligibilityManagement: {
+        scheduled:{
+            min: 50,
+            max: 150,
+        },
+        wait:{
+            min: 50,
+            max: 150,
+        },
+        inProgress:{
+            min: 50,
+            max: 150,
+        },
+        failed:{
+            min: 50,
+            max: 150,
+        },
+        needsIntervention:{
+            min: 50,
+            max: 150,
+        },
+        completed:{
+            min: 100,
+            max: 150,
+        }
+    },
+    AlertManagement: {
         scheduled:{
             min: 50,
             max: 150,
