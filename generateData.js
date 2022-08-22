4c39cd9b9bc47e0aa920789e87aa7beda366418c
Himanshu Raj <himanshurajverma549@gmail.com>
2022-08-22 18:01:12 +0530

    modified data to match the dropdowns

--- generateData.js
+++ generateData.js
@@ -42,7 +42,8 @@ class GENERATEDATA {
     }
 
     randomIntFromInterval(min, max) { // min and max included 
-        return Math.floor(Math.random() * (max - min + 1) + min)
+        let number = Math.floor(Math.random() * (max - min + 1) + min)
+        return number.toLocaleString();
     }
 
     randomIntArrayInRange(min, max, n = 1) {
@@ -59,76 +60,14 @@ class GENERATEDATA {
             date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
             expires = "; expires=" + date.toUTCString();
         }
-        document.cookie = name + "=" + (value || "") + expires + "; path=/";
+        localStorage.setItem(name, value)
+        document.cookie = name + "=" + ('onlyExp' || "") + expires + "; path=/";
     }
 
 }
 
 const AnalyticsData = {
-    RCD: {
-        TransactionsCompleted: {
-            numberOfTransctionsRange: {
-                min: 2000,
-                max: 3500,
-            },
-            percentFromLastRange: {
-                min: 10,
-                max: 40,
-            },
-        },
-        NeedsInterventions: {
-            numberOfTransctionsRange: {
-                min: 200,
-                max: 400,
-            },
-            percentFromLastRange: {
-                min: 2,
-                max: 15,
-            },
-        },
-        haveSaved: {
-            hours: {
-                min: 50,
-                max: 200,
-            },
-            money: {
-                min: 10990,
-                max: 12990,
-            },
-            FTE: {
-                min: 2.5,
-                max: 6.6,
-            }
-        },
-        ThroughputPerHour: {
-            transactions: {
-                min: 233,
-                max: 600,
-            },
-            avgMinTransctions: {
-                min: 1,
-                max: 10,
-            }
-        },
-        chartOneDataPoints: {
-            byDay:{
-                rangeMin: 100,
-                rangeMax: 400,
-                length: 30,
-            },
-            byWeek:{
-                rangeMin: 200,
-                rangeMax: 400,
-                length: 30,
-            }
-        },
-        chartTwoDataPoints: {
-            rangeMin: 0,
-            rangeMax: 50,
-            length: 11,
-        },
-    },
-    Authorization: {
+    Authorizations: {
         TransactionsCompleted: {
             numberOfTransctionsRange: {
                 min: 2000,
@@ -380,6 +319,195 @@ const AnalyticsData = {
             length: 11,
         },
     },
+    MedicareEligibilityAlertMgmt: {
+        TransactionsCompleted: {
+            numberOfTransctionsRange: {
+                min: 2000,
+                max: 7470,
+            },
+            percentFromLastRange: {
+                min: 10,
+                max: 40,
+            },
+        },
+        NeedsInterventions: {
+            numberOfTransctionsRange: {
+                min: 200,
+                max: 400,
+            },
+            percentFromLastRange: {
+                min: 2,
+                max: 15,
+            },
+        },
+        haveSaved: {
+            hours: {
+                min: 50,
+                max: 200,
+            },
+            money: {
+                min: 10990,
+                max: 12990,
+            },
+            FTE: {
+                min: 2.5,
+                max: 6.6,
+            }
+        },
+        ThroughputPerHour: {
+            transactions: {
+                min: 233,
+                max: 600,
+            },
+            avgMinTransctions: {
+                min: 1,
+                max: 10,
+            }
+        },
+        chartOneDataPoints: {
+            byDay:{
+                rangeMin: 100,
+                rangeMax: 400,
+                length: 30,
+            },
+            byWeek:{
+                rangeMin: 200,
+                rangeMax: 400,
+                length: 30,
+            }
+        },
+        chartTwoDataPoints: {
+            rangeMin: 0,
+            rangeMax: 50,
+            length: 11,
+        },
+    },
+    'Physician/FacilityNotification': {
+        TransactionsCompleted: {
+            numberOfTransctionsRange: {
+                min: 2000,
+                max: 7470,
+            },
+            percentFromLastRange: {
+                min: 10,
+                max: 40,
+            },
+        },
+        NeedsInterventions: {
+            numberOfTransctionsRange: {
+                min: 200,
+                max: 400,
+            },
+            percentFromLastRange: {
+                min: 2,
+                max: 15,
+            },
+        },
+        haveSaved: {
+            hours: {
+                min: 50,
+                max: 200,
+            },
+            money: {
+                min: 10990,
+                max: 12990,
+            },
+            FTE: {
+                min: 2.5,
+                max: 6.6,
+            }
+        },
+        ThroughputPerHour: {
+            transactions: {
+                min: 233,
+                max: 600,
+            },
+            avgMinTransctions: {
+                min: 1,
+                max: 10,
+            }
+        },
+        chartOneDataPoints: {
+            byDay:{
+                rangeMin: 100,
+                rangeMax: 400,
+                length: 30,
+            },
+            byWeek:{
+                rangeMin: 200,
+                rangeMax: 400,
+                length: 30,
+            }
+        },
+        chartTwoDataPoints: {
+            rangeMin: 0,
+            rangeMax: 50,
+            length: 11,
+        },
+    },
+    OASIStoiQIES: {
+        TransactionsCompleted: {
+            numberOfTransctionsRange: {
+                min: 2000,
+                max: 7470,
+            },
+            percentFromLastRange: {
+                min: 10,
+                max: 40,
+            },
+        },
+        NeedsInterventions: {
+            numberOfTransctionsRange: {
+                min: 200,
+                max: 400,
+            },
+            percentFromLastRange: {
+                min: 2,
+                max: 15,
+            },
+        },
+        haveSaved: {
+            hours: {
+                min: 50,
+                max: 200,
+            },
+            money: {
+                min: 10990,
+                max: 12990,
+            },
+            FTE: {
+                min: 2.5,
+                max: 6.6,
+            }
+        },
+        ThroughputPerHour: {
+            transactions: {
+                min: 233,
+                max: 600,
+            },
+            avgMinTransctions: {
+                min: 1,
+                max: 10,
+            }
+        },
+        chartOneDataPoints: {
+            byDay:{
+                rangeMin: 100,
+                rangeMax: 400,
+                length: 30,
+            },
+            byWeek:{
+                rangeMin: 200,
+                rangeMax: 400,
+                length: 30,
+            }
+        },
+        chartTwoDataPoints: {
+            rangeMin: 0,
+            rangeMax: 50,
+            length: 11,
+        },
+    },
 }
 const WorkFlowSummeryData = {
     RCD: {
@@ -408,7 +536,7 @@ const WorkFlowSummeryData = {
             max: 150,
         }
     },
-    Authorization: {
+    Authorizations: {
         scheduled:{
             min: 50,
             max: 150,
@@ -487,6 +615,84 @@ const WorkFlowSummeryData = {
         }
     },
     AlertManagement: {
+        scheduled:{
+            min: 50,
+            max: 150,
+        },
+        wait:{
+            min: 50,
+            max: 150,
+        },
+        inProgress:{
+            min: 50,
+            max: 150,
+        },
+        failed:{
+            min: 50,
+            max: 150,
+        },
+        needsIntervention:{
+            min: 50,
+            max: 150,
+        },
+        completed:{
+            min: 100,
+            max: 150,
+        }
+    },
+    MedicareEligibilityAlertMgmt: {
+        scheduled:{
+            min: 50,
+            max: 150,
+        },
+        wait:{
+            min: 50,
+            max: 150,
+        },
+        inProgress:{
+            min: 50,
+            max: 150,
+        },
+        failed:{
+            min: 50,
+            max: 150,
+        },
+        needsIntervention:{
+            min: 50,
+            max: 150,
+        },
+        completed:{
+            min: 100,
+            max: 150,
+        }
+    },
+    'Physician/FacilityNotification': {
+        scheduled:{
+            min: 50,
+            max: 150,
+        },
+        wait:{
+            min: 50,
+            max: 150,
+        },
+        inProgress:{
+            min: 50,
+            max: 150,
+        },
+        failed:{
+            min: 50,
+            max: 150,
+        },
+        needsIntervention:{
+            min: 50,
+            max: 150,
+        },
+        completed:{
+            min: 100,
+            max: 150,
+        }
+    },
+    OASIStoiQIES: {
         scheduled:{
             min: 50,
             max: 150,
